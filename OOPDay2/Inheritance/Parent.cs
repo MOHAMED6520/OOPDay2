bce8cf2681a1dd8b7d9e0c0d267dab1e0fdf306c
moham <moham@DESKTOP-M8CT1H1>
2025-07-26 14:19:06 +0300

    Inheritance

---
+++ OOPDay2/Inheritance/Parent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDay2.Inheritance
+{
+    internal class Parent
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Parent(int x , int y) 
+        { 
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"X = {X} :: Y = {Y}";
+        }
+
+        public void Fun01()
+        {
+            Console.WriteLine("I am Parent [base] ");
+        }
+
+        public void Fun02()
+        {
+            Console.WriteLine($"X = {X} :: Y = {Y}");
+        }
+    }
+}
--- OOPDay2/Program.cs
+++ OOPDay2/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace OOPDay2
+﻿using OOPDay2.Inheritance;
+
+namespace OOPDay2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-          
+
+
+            #region Inheritance
+            //// Inheritance
+            //Parent parent = new Parent(1,2);
+            //Console.WriteLine(parent);
+            //parent.Fun01();
+            //parent.Fun02();
+
+            //Child child = new Child(1,2, 3);
+            //child.Fun01();
+            //child.Fun02(); 
+            #endregion
         }
     }
 }
